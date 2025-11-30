ea4a049b8692644fc189bca32ed11df99343762f
Teo <teo21maitr@gmail.com>
2025-11-30 22:47:47 +0100

    fix: serve frontend index.html from django

--- backend/teamshop/settings.py
+++ backend/teamshop/settings.py
@@ -68,7 +68,8 @@ ROOT_URLCONF = "teamshop.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "staticfiles"],
+
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- backend/teamshop/urls.py
+++ backend/teamshop/urls.py
@@ -17,8 +17,14 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
+    # Serve React Frontend for any other route
+    path("", TemplateView.as_view(template_name="index.html")),
+    path("<path:resource>", TemplateView.as_view(template_name="index.html")),
 ]
+
